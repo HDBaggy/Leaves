d50299498f8ce91452f694f253d66083e591613e
HarshitDaftary <daftaryharshit@yahoo.co.in>
2017-08-19 09:11:00 +0530

    fixed compile error

---
+++ Wikileaf/Wikileaf/CustomViews/DrawerView/DispensaryMenuHeaderView.swift
@@ -0,0 +1,19 @@
+//
+//  DispensaryMenuHeaderView.swift
+//  Wikileaf
+//
+//  Created by Harshit on 09/08/17.
+//  Copyright © 2017 ZestyGuys. All rights reserved.
+//
+
+import UIKit
+
+class DispensaryMenuHeaderView: UIView {
+
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var btnUnit: UIButton!
+    
+    @IBAction func btnUnitTapped(_ sender: Any) {
+
+    }
+}
